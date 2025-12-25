92146f94b39a51e1118f51327c6c289702b0e837
bikash-kumar-kc <kcbikash886@gmail.com>
2025-12-25 12:39:47 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Tiptap from './tiptap/Tiptap'
+
+function App() {
+ 
+
+  return (
+  <div className=' mt-4'>
+  <Tiptap/>
+  </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import system from './theme'
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+   <ChakraProvider value={system}>
+     <App />
+   </ChakraProvider>
+  // </StrictMode>,
+)
